d2f1dc853e21721065df7ff141609a16c61bed16
viper1788 <victorpermana@gmail.com>
2026-06-10 15:03:30 +0700

    tell vite config to treat all js as jsx

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist'
-  }
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
