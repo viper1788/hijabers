a07defec0d98087def476a375ebef648957c41a9
viper1788 <victorpermana@gmail.com>
2026-06-24 11:51:14 +0700

    update supabasejs

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = 'https://lqwbgilymqkxszjwinls.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_5fvrUPSnmx-T-h9-g_XCcQ_XFKCDeRU'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+})
