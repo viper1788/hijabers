123d281e8e57908ae6bf5851889619e5569178c2
null <null>
2026-06-24 12:25:50 +0700

    update supabase anonkey

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://lqwbgilymqkxszjwinls.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_5fvrUPSnmx-T-h9-g_XCcQ_XFKCDeRU'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxxd2JnaWx5bXFreHN6andpbmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NTg4NjMsImV4cCI6MjA5NjQzNDg2M30.U2LLmrthYTYkIBht8MboOrgkJbvZSo4gFhom-y3NsSo'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
